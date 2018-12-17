6661b2b04e943d299a777f6d4d88722d588b2e10
Joseph Marhee <jmarhee@interiorae.com>
2018-12-16 20:00:33 -0600

    fixes rename

---
+++ 2-kube-controller.tf
@@ -0,0 +1,21 @@
+data "template_file" "controller" {
+  template = "${file("${path.module}/controller.tpl")}"
+
+  vars {
+    kube_token   = "${random_string.kube_init_token_a.result}.${random_string.kube_init_token_b.result}"
+    do_token     = "${var.digitalocean_token}"
+    ccm_version  = "${var.digitalocean_ccm_release}"
+    kube_version = "${var.kubernetes_version}"
+  }
+}
+
+resource "digitalocean_droplet" "k8s_primary" {
+  name      = "${var.cluster_name}-primary"
+  image     = "ubuntu-16-04-x64"
+  size      = "${var.primary_size}"
+  region    = "${var.region}"
+  backups   = "true"
+  private_networking = "true"
+  ssh_keys  = "${var.ssh_key_fingerprints}"
+  user_data = "${data.template_file.controller.rendered}"
+}
